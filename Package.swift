e7db5b0969602da5a43025fdc96167dc107b123d
Erick Del Orbe <erick@har.com>
2024-10-09 19:38:46 -0500

    Trying to make SPM work

--- Package.swift
+++ Package.swift
@@ -21,7 +21,12 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "JSQMessagesViewController", path: "JSQMessagesViewController", cSettings: [.headerSearchPath("JSQMessagesViewController")]),
+        .target(name: "JSQMessagesViewController", path: "JSQMessagesViewController", cSettings: [
+            .headerSearchPath("JSQMessagesViewController"),
+            .headerSearchPath("JSQMessagesViewController/Views"),
+            .headerSearchPath("JSQMessagesViewController/Factories"),
+            .headerSearchPath("JSQMessagesViewController/Categories")
+                                                                                                 ]),
     ],
     swiftLanguageVersions: [.version("5")]
 )
